f9cc3adc42bed037296c892034036669463663f9
Maurog-rgba <gabriel.mauro.santos.3@gmail.com>
2024-04-02 14:24:30 -0300

    Feat: function to play and pause video on home page

--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  @ViewChild('Video') Video!: ElementRef<HTMLVideoElement>;
 
+  isStopped = false;
+
+  stopVideoPlayer() {
+    this.isStopped = !this.isStopped;
+    if (this.isStopped) {
+      this.Video.nativeElement.pause();
+    } else {
+      this.Video.nativeElement.play();
+    }
+  }
 }
