41bf7690c6c235057829a24adcdd11570669f441
Maurog-rgba <gabriel.mauro.santos.3@gmail.com>
2024-04-04 11:57:29 -0300

    Responsive: Add responsiveness to header component logo

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  logo: 'desktop' | 'mobile' = 'mobile';
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenSize();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    this.checkScreenSize();
+  }
+
+  checkScreenSize() {
+    this.logo = window.innerWidth <= 768 ? 'mobile' : 'desktop';
+  }
 }
